20ac3d88b715e4bec1ae922540a0520a4c4fab79
aheadley <aheadley@nexcess.net>
2013-03-05 14:43:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# [DRM Facts Website](http://drm-facts.info/)
+
+Code and content backing the DRM Facts site.
+
+### License
+
+Code is licensed under the ISC, see `LICENSE.code` for more details. Site
+content is licensed under CC BY-NC-SA 3.0, see `LICENSE.content` for more details.
---
+++ src/build.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2013, Alex Headley <aheadley@waysaboutstuff.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any purpose
+# with or without fee is hereby granted, provided that the above copyright notice
+# and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+# THIS SOFTWARE.
+
+import os.path
+
+import staticjinja
+
+TEMPLATE_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'templates')
+OUTPUT_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'output')
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    parser = OptionParser()
+
+    parser.add_option('-t', '--template', dest='searchpath',
+        help='Path to template directory', metavar='PATH',
+        default=TEMPLATE_DIR)
+    parser.add_option('-o', '--output', dest='outpath',
+        help='Path to generated output', metavar='PATH',
+        default=OUTPUT_DIR)
+    opts, args = parser.parse_args()
+
+    staticjinja.main(searchpath=opts.searchpath, outpath=opts.outpath)
